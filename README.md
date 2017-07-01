7657283f161295b426c1e91711f046073725a651
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2017-07-01 12:03:56 +0200

    Initial commit

--- README.md
+++
@@ -1,7 +0,0 @@
-# PkgHelp
-
-[![Build Status](https://travis-ci.org/jagot/PkgHelp.jl.svg?branch=master)](https://travis-ci.org/jagot/PkgHelp.jl)
-
-[![Coverage Status](https://coveralls.io/repos/jagot/PkgHelp.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/jagot/PkgHelp.jl?branch=master)
-
-[![codecov.io](http://codecov.io/github/jagot/PkgHelp.jl/coverage.svg?branch=master)](http://codecov.io/github/jagot/PkgHelp.jl?branch=master)
--- src/PkgHelp.jl
+++ src/PkgHelp.jl
@@ -1,5 +1,47 @@
 module PkgHelp
 
-# package code goes here
+using PkgDev
+
+function get_sync_dir()
+    pkghelp_conf = expanduser("~/.julia/pkghelp.conf")
+    sync_dir = if isfile(pkghelp_conf)
+        readstring(pkghelp_conf)
+    else
+        f = ""
+        while !isdir(f)
+            print("Input Julia pkg sync dir: ")
+            f = expanduser(strip(readline(STDIN)))
+        end
+        open(pkghelp_conf, "w") do file
+            write(file, f)
+        end
+        f
+    end
+end
+
+function generate(pkg_name, license; kwargs...)
+    ismatch(r"\.jl$", pkg_name) && (pkg_name = pkg_name[1:end-3])
+
+    PkgDev.generate(pkg_name, license; kwargs...)
+
+    pkg_dir = Pkg.dir(pkg_name)
+    pkg_sync_dir = joinpath(get_sync_dir(), "$(pkg_name).jl")
+
+    println("Moving $(pkg_dir) -> $(pkg_sync_dir)")
+    mv(pkg_dir, pkg_sync_dir)
+
+    link(pkg_name)
+end
+
+function link(pkg_name)
+    ismatch(r"\.jl$", pkg_name) && (pkg_name = pkg_name[1:end-3])
+
+    pkg_dir = Pkg.dir(pkg_name)
+    pkg_sync_dir = joinpath(get_sync_dir(), "$(pkg_name).jl")
+
+    println("Linking $(pkg_sync_dir) -> $(pkg_dir)")
+    symlink(pkg_sync_dir, pkg_dir)
+end
+
 
 end # module
