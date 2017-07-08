72f9bc79a61cecf6ded8d782bd9b3700195dcf21
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2017-07-08 16:41:34 +0200

    Modularized setup

--- src/PkgHelp.jl
+++ src/PkgHelp.jl
@@ -4,27 +4,97 @@ using PkgDev
 using YAML
 using Coverage
 
-function get_config()
+function get_config(fun::Function, variable::String)
     pkghelp_conf = expanduser("~/.julia/pkghelp.yml")
-    sync_dir = if isfile(pkghelp_conf)
-        YAML.load(open(pkghelp_conf))
-    else
+    config = isfile(pkghelp_conf) ? YAML.load(open(pkghelp_conf)) : Dict()
+
+    if variable in keys(config)
+        return config[variable]
+    end
+
+    config[variable] = fun()
+
+    open(pkghelp_conf, "w") do file
+        for k in keys(config)
+            write(file, "$(k): $(config[k])\n")
+        end
+    end
+
+    config[variable]
+end
+
+function get_syncdir()
+    get_config("dir") do
         f = ""
         while !isdir(f)
             print("Input Julia pkg sync dir: ")
             f = expanduser(strip(readline(STDIN)))
         end
-        config = Dict("dir" => f)
+        f
+    end
+end
 
+function get_readme()
+    get_config("readme") do
         print("Change default README format from Markdown to Org? [Y/n] ")
-        config["readme"] = lowercase(strip(readline())) == "n" ? "markdown" : "org"
+        lowercase(strip(readline())) == "n" ? "markdown" : "org"
+    end
+end
 
-        open(pkghelp_conf, "w") do file
-            for k in keys(config)
-                write(file, "$(k): $(config[k])\n")
-            end
+function get_git_config(variable)
+    try
+        strip(readstring(`git config --get $(variable)`))
+    catch
+        ""
+    end
+end
+
+function get_github_user()
+    github_user = get_git_config("github.user")
+    if github_user == ""
+        print("Enter your Github username: ")
+        github_user = strip(readline(STDIN))
+        run(`git config --global github.user $(github_user)`)
+    end
+    github_user
+end
+
+function set_git_ssh(dir)
+    cd(dir) do
+        git_url = get_git_config("remote.origin.url")
+        m = match(r"https://github.com/(.+)/(.+)", git_url)
+        if m != nothing
+            new_git_url = "git@github.com:$(m[1])/$(m[2])"
+            println("Remote url $(git_url) -> $(new_git_url)")
+            run(`git remote set-url origin $(new_git_url)`)
         end
-        config
+    end
+end
+
+function org_readme(dir, pkg_name)
+    cd(dir) do
+        open("README.org", "w") do file
+            write(file, "#+TITLE: $(pkg_name).jl\n")
+            user_name = get_git_config("user.name")
+            user_name != "" && write(file, "#+AUTHOR: $(user_name)\n")
+            user_email = get_git_config("user.email")
+            user_email != "" && write(file, "#+EMAIL: $(user_email)\n")
+            github_user = get_github_user()
+            write(file, "\n")
+            for (url,badge) in
+                [("https://travis-ci.org/$(github_user)/$(pkg_name).jl",
+                  "https://travis-ci.org/$(github_user)/$(pkg_name).jl.svg?branch=master"),
+                 ("https://coveralls.io/github/$(github_user)/$(pkg_name).jl?branch=master",
+                  "https://coveralls.io/repos/$(github_user)/$(pkg_name).jl/badge.svg?branch=master&service=github"),
+                 ("http://codecov.io/github/$(github_user)/$(pkg_name).jl?branch=master",
+                  "http://codecov.io/github/$(github_user)/$(pkg_name).jl/coverage.svg?branch=master")]
+                write(file, "[[$(url)][$(badge)]]\n")
+            end
+            write(file, "\n")
+        end
+        rm("README.md")
+        run(`git add README.org`)
+        run(`git commit -a -m "README.md -> README.org"`)
     end
 end
 
@@ -33,58 +103,24 @@ function generate(pkg_name, license; kwargs...)
 
     PkgDev.generate(pkg_name, license; kwargs...)
 
-    config = get_config()
-
     pkg_dir = Pkg.dir(pkg_name)
-    pkg_sync_dir = joinpath(config["dir"], "$(pkg_name).jl")
+    pkg_sync_dir = joinpath(get_syncdir(), "$(pkg_name).jl")
 
     println("Moving $(pkg_dir) -> $(pkg_sync_dir)")
     mv(pkg_dir, pkg_sync_dir)
 
     link(pkg_name)
 
-    cd(pkg_sync_dir) do
-        git_url = strip(readstring(`git config --get remote.origin.url`))
-        m = match(r"https://github.com/(.+)/(.+)", git_url)
-        if m != nothing
-            new_git_url = "git@github.com:$(m[1])/$(m[2])"
-            println("Remote url $(git_url) -> $(new_git_url)")
-            run(`git remote set-url origin $(new_git_url)`)
-        end
-
-        if config["readme"] == "org"
-            open("README.org", "w") do file
-                write(file, "#+TITLE: $(pkg_name).jl\n")
-                user_name = strip(readstring(`git config --get user.name`))
-                user_name != "" && write(file, "#+AUTHOR: $(user_name)\n")
-                user_email = strip(readstring(`git config --get user.email`))
-                user_email != "" && write(file, "#+EMAIL: $(user_email)\n")
-                write(file, "\n")
-                for (url,badge) in
-                    [("https://travis-ci.org/jagot/$(pkg_name).jl",
-                      "https://travis-ci.org/jagot/$(pkg_name).jl.svg?branch=master"),
-                     ("https://coveralls.io/github/jagot/$(pkg_name).jl?branch=master",
-                      "https://coveralls.io/repos/jagot/$(pkg_name).jl/badge.svg?branch=master&service=github"),
-                     ("http://codecov.io/github/jagot/$(pkg_name).jl?branch=master",
-                      "http://codecov.io/github/jagot/$(pkg_name).jl/coverage.svg?branch=master")]
-                    write(file, "[[$(url)][$(badge)]]\n")
-                end
-                write(file, "\n")
-            end
-            rm("README.md")
-            run(`git add README.org`)
-            run(`git commit -a -m "README.md -> README.org"`)
-        end
-    end
+    get_github_user()
+    set_git_ssh(pkg_sync_dir)
+    get_readme() == "org" && org_readme(pkg_sync_dir, pkg_name)
 end
 
 function link(pkg_name)
     ismatch(r"\.jl$", pkg_name) && (pkg_name = pkg_name[1:end-3])
 
-    config = get_config()
-
     pkg_dir = Pkg.dir(pkg_name)
-    pkg_sync_dir = joinpath(config["dir"], "$(pkg_name).jl")
+    pkg_sync_dir = joinpath(get_syncdir(), "$(pkg_name).jl")
 
     println("Linking $(pkg_sync_dir) -> $(pkg_dir)")
     symlink(pkg_sync_dir, pkg_dir)
